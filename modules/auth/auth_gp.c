50d4a5f818590995dde09d08c1313efcd17171bf
Alexander Kalkhof <alex@kalkhof.org>
2020-03-19 12:41:55 +0100

    update

--- modules/auth/auth_gp.c
+++ modules/auth/auth_gp.c
@@ -44,6 +44,7 @@ int auth_gp_get (AuthGP* p_self, QBusM qin, QBusM qout, CapeErr err)
   
   // local vars (initialization)
   CapeUdc query_results = NULL;
+  number_t gpid = 0;
 
   // do some security checks
   if (qin->rinfo == NULL)
@@ -53,11 +54,30 @@ int auth_gp_get (AuthGP* p_self, QBusM qin, QBusM qout, CapeErr err)
   }
   
   self->wpid = cape_udc_get_n (qin->rinfo, "wpid", 0);
-  
   if (self->wpid == 0)
   {
     res = cape_err_set (err, CAPE_ERR_NO_AUTH, "missing wpid");
     goto exit_and_cleanup;
+  }
+  
+  gpid = cape_udc_get_n (qin->rinfo, "gpid", 0);
+  if (gpid == 0)
+  {
+    res = cape_err_set (err, CAPE_ERR_NO_AUTH, "missing gpid");
+    goto exit_and_cleanup;
+  }
+
+  // check role
+  {
+    CapeUdc roles = cape_udc_get (qin->rinfo, "roles");
+    if (roles)
+    {
+      CapeUdc role_admin = cape_udc_get (roles, "wspc_admin");
+      if (role_admin)
+      {
+        gpid = 0;
+      }
+    }
   }
 
   {
@@ -66,11 +86,83 @@ int auth_gp_get (AuthGP* p_self, QBusM qin, QBusM qout, CapeErr err)
 
     cape_udc_add_n      (params, "wpid"        , self->wpid);
     
+    if (gpid)
+    {
+      cape_udc_add_n    (params, "gpid"        , gpid);
+    }
+    
     cape_udc_add_n      (values, "gpid"        , 0);
     cape_udc_add_n      (values, "userid"      , 0);
     cape_udc_add_s_cp   (values, "title"       , NULL);
     cape_udc_add_s_cp   (values, "firstname"   , NULL);
     cape_udc_add_s_cp   (values, "lastname"    , NULL);
+
+    // execute the query
+    query_results = adbl_session_query (self->adbl_session, "rbac_users_view", &params, &values, err);
+    if (query_results == NULL)
+    {
+      res = cape_err_code (err);
+      goto exit_and_cleanup;
+    }
+  }
+  
+  cape_udc_replace_mv (&(qout->cdata), &query_results);
+  res = CAPE_ERR_NONE;
+  
+exit_and_cleanup:
+  
+  cape_udc_del (&query_results);
+  
+  auth_gp_del (p_self);
+  return res;
+}
+
+//-----------------------------------------------------------------------------
+
+int auth_gp_account (AuthGP* p_self, QBusM qin, QBusM qout, CapeErr err)
+{
+  int res;
+  AuthGP self = *p_self;
+  
+  // local vars (initialization)
+  CapeUdc query_results = NULL;
+  number_t gpid = 0;
+
+  // do some security checks
+  if (qin->rinfo == NULL)
+  {
+    res = cape_err_set (err, CAPE_ERR_NO_AUTH, "missing rinfo");
+    goto exit_and_cleanup;
+  }
+  
+  self->wpid = cape_udc_get_n (qin->rinfo, "wpid", 0);
+  if (self->wpid == 0)
+  {
+    res = cape_err_set (err, CAPE_ERR_NO_AUTH, "missing wpid");
+    goto exit_and_cleanup;
+  }
+  
+  gpid = cape_udc_get_n (qin->rinfo, "gpid", 0);
+  if (gpid == 0)
+  {
+    res = cape_err_set (err, CAPE_ERR_NO_AUTH, "missing gpid");
+    goto exit_and_cleanup;
+  }
+
+  {
+    CapeUdc params = cape_udc_new (CAPE_UDC_NODE, NULL);
+    CapeUdc values = cape_udc_new (CAPE_UDC_NODE, NULL);
+
+    cape_udc_add_n      (params, "wpid"        , self->wpid);
+    cape_udc_add_n      (params, "gpid"        , gpid);
+    
+    cape_udc_add_n      (values, "gpid"        , 0);
+    cape_udc_add_n      (values, "userid"      , 0);
+    cape_udc_add_s_cp   (values, "title"       , NULL);
+    cape_udc_add_s_cp   (values, "firstname"   , NULL);
+    cape_udc_add_s_cp   (values, "lastname"    , NULL);
+    cape_udc_add_s_cp   (values, "workspace"   , NULL);
+    cape_udc_add_s_cp   (values, "secret"      , NULL);
 
     // execute the query
     query_results = adbl_session_query (self->adbl_session, "rbac_users_view", &params, &values, err);
--- modules/auth/auth_gp.h
+++ modules/auth/auth_gp.h
@@ -17,13 +17,15 @@ struct AuthGP_s; typedef struct AuthGP_s* AuthGP;
 
 //-----------------------------------------------------------------------------
 
-__CAPE_LIBEX   AuthGP   auth_gp_new    (AdblSession adbl_session);
+__CAPE_LIBEX   AuthGP   auth_gp_new       (AdblSession adbl_session);
 
-__CAPE_LIBEX   void     auth_gp_del    (AuthGP*);
+__CAPE_LIBEX   void     auth_gp_del       (AuthGP*);
 
 //-----------------------------------------------------------------------------
 
-__CAPE_LIBEX   int      auth_gp_get     (AuthGP*, QBusM qin, QBusM qout, CapeErr err);
+__CAPE_LIBEX   int      auth_gp_get       (AuthGP*, QBusM qin, QBusM qout, CapeErr err);
+
+__CAPE_LIBEX   int      auth_gp_account   (AuthGP*, QBusM qin, QBusM qout, CapeErr err);
 
 //-----------------------------------------------------------------------------
 
