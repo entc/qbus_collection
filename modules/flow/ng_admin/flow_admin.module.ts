3a635156622890a5f2a7cda347c4f7c4ad8a24bc
Alexander Kalkhof <alex@kalkhof.org>
2020-03-23 08:09:43 +0100

    renamed ng to ng_admin

--- modules/flow/ng_admin/flow_admin.module.ts
+++ modules/flow/ng_admin/flow_admin.module.ts
@@ -4,16 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { FlowEditorComponent } from './flow-editor/flow-editor.component';
 import { FlowListComponent } from './flow-list/flow-list.component';
+import { FlowProcessComponent } from './flow-process/flow-process.component';
 
 const routes: Routes = [
   { path: 'flow_editor', component: FlowEditorComponent },
-  { path: 'flow_editor/:wfid', component: FlowListComponent }
+  { path: 'flow_editor/:wfid', component: FlowListComponent },
+  { path: 'flow_process', component: FlowProcessComponent }
 ];
 
 @NgModule({
+  declarations: [ FlowListComponent, FlowEditorComponent, FlowProcessComponent],
   imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class FlowRoutingModule
+export class FlowAdminModule
 {
 }
