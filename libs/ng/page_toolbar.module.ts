b59ac312f852f4bb1347e7c786d60197f06de6c6
Alexander Kalkhof <a.kalkhof@smartscribe.de>
2022-09-23 00:13:05 +0200

    cosmetics

--- libs/ng/page_toolbar.module.ts
+++ libs/ng/page_toolbar.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { PageToolbarPipe, QbngSortPipe, QbngFilterPipe, PageToolbarComponent, RxModelDirective } from './page_toolbar/component';
+import { PageToolbarPipe, QbngSortPipe, QbngFilterPipe, PageToolbarComponent, RxModelDirective, QbngFSPipeFile } from './page_toolbar/component';
 import { NgxPaginationModule, PaginatePipe } from 'ngx-pagination';
 import { PageService } from './page_service/service';
 
 @NgModule({
-  declarations: [PageToolbarPipe, QbngSortPipe, QbngFilterPipe, PageToolbarComponent, RxModelDirective],
+  declarations: [PageToolbarPipe, QbngSortPipe, QbngFilterPipe, PageToolbarComponent, RxModelDirective, QbngFSPipeFile],
   imports: [CommonModule, FormsModule, NgxPaginationModule],
-  exports: [PageToolbarPipe, QbngSortPipe, QbngFilterPipe, PageToolbarComponent, PaginatePipe, RxModelDirective],
+  exports: [PageToolbarPipe, QbngSortPipe, QbngFilterPipe, PageToolbarComponent, PaginatePipe, RxModelDirective, QbngFSPipeFile],
   providers: [ PageService ]
 })
 export class PageToolbarModule
