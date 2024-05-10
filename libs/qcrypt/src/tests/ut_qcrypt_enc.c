19a3eb838ccb945b17a8c3d8c4ddae73f805a9b8
Alexander Kalkhof <alex@kalkhof.org>
2024-05-10 16:39:28 +0200

    merged

---
+++ libs/qcrypt/src/tests/ut_qcrypt_enc.c
@@ -0,0 +1,60 @@
+#include <qcrypt.h>
+
+// cape includes
+#include <sys/cape_log.h>
+#include <fmt/cape_json.h>
+#include <stc/cape_str.h>
+
+//-----------------------------------------------------------------------------
+
+int main (int argc, char *argv[])
+{
+  int res;
+  CapeErr err = cape_err_new ();
+  
+  CapeString enc01 = qcrypt__hash_sha256__hex_o ("test42", 6, err);
+  CapeString enc02 = qcrypt__hash_md5__hex_o ("test42", 6, err);
+  CapeString enc03 = qcrypt__hash_sha__hex_o ("test42", 6, err);
+
+  printf ("SHA254: %s\n", enc01);
+  printf ("SHA1  : %s\n", enc03);
+  printf ("MD5   : %s\n", enc02);
+
+  if (FALSE == cape_str_equal (enc01, "338c0605bab38900480ebcc7fb0651426cc26cd1732579f04b47f779a8962d83"))
+  {
+    res = cape_err_set (err, CAPE_ERR_WRONG_VALUE, "SHA254 missmatch");
+    goto exit_and_cleanup;
+  }
+
+  if (FALSE == cape_str_equal (enc02, "a681d0039910f6b38666d5cbc2e5ac1d"))
+  {
+    res = cape_err_set (err, CAPE_ERR_WRONG_VALUE, "MD5 missmatch");
+    goto exit_and_cleanup;
+  }
+
+  if (FALSE == cape_str_equal (enc03, "3205f150ab938e497f8e79b4da95db96c1ea0fe6"))
+  {
+    res = cape_err_set (err, CAPE_ERR_WRONG_VALUE, "SHA1 missmatch");
+    goto exit_and_cleanup;
+  }
+
+
+  
+  res = CAPE_ERR_NONE;
+  
+exit_and_cleanup:
+  
+  if (cape_err_code(err))
+  {
+    cape_log_fmt (CAPE_LL_ERROR, "TEST", "MAIN", "test failed: %s", cape_err_text (err));
+  }
+  
+  cape_str_del (&enc01);
+  cape_str_del (&enc02);
+  cape_str_del (&enc03);
+
+  cape_err_del (&err);
+  
+  return res;
+}
+
